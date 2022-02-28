44c9771e2a0ab0e1c2be2a8440cb236507d5ee3d
gustavorodri2011 <gustavorodri2011@gmail.com>
2022-02-28 02:46:49 -0300

    Finish Date class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConceptsPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(new Date(2024, 2, 29));
+                Console.WriteLine(new Date(1974, 10, 05));
+                Console.WriteLine(new Date(2005, 11, 30));
+            }
+            catch (Exception error)
+            {
+
+                Console.WriteLine(error.Message);
+            }
+        }
+    }
+}
